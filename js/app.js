f64319c4b230a896d181ed5480c1aefaea0a4305
Dima <topface26007@gmail.com>
2021-02-02 22:32:28 +0200

    123

---
+++ js/app.js
@@ -0,0 +1,80 @@
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+        /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 1500);
+    });
+
+
+    /* Slider */
+$(document).ready(function(){
+  $('.comment__right').slick({
+    infinite: true,
+    speed:800,
+    vertical:true,
+    verticalSwiping:true,
+  rows:2,
+  });
+});
+
+
+
+    /* Modal
+    =====================*/
+
+    const modalCall = $("[data-modal]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+
+        worksSlider.slick('setPosition');
+    });
+
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
+    });
